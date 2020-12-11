a0cd1eaba06720997836fa70d0b612a98df2afb1
Justin Korn <justinkorn@Justins-MacBook-Pro.local>
2020-12-10 22:07:32 -0800

    Adding classes

---
+++ CanvasProjectClasses.js
@@ -0,0 +1,16 @@
+var x1 = 560;
+var y1 = 270;
+var obstacleMovement = -1;
+var r = Math.floor(Math.random() * 50)
+
+class obstacles {
+  constructor(x1, y1, 30, r) {
+    this.beginPath();
+    this.fillStyle = "ff0000";
+    this.fill();
+  }
+}
+
+generateTerrain() {
+  this.x1 += obstacleMovment;
+}
